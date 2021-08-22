3b7fac635c30b55d2cf3da245b1bea7e4667082b
Maks-Katunin <katunin87@gmail.com>
2021-08-23 01:03:02 +0600

    Initial commit

---
+++ src/com/company/Hero.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public abstract class Hero implements HavingSuperAbility {
+    private int health;
+    private int damage;
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HavingSuperAbility[] heroes = {new Medic(), new Magic(), new Warrior()};
+        for (int i = 0; i < heroes.length; i++) {
+
+            heroes[i].applySuperAbility("SuperAbility");
+
+        }
+    }
+
+}
